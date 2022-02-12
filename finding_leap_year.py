1710812f4f4192b24710d30e024d3b09638d2ece
Abdullah Tushar <aamtushar09@gmail.com>
2022-02-12 22:59:00 +0600

    leap year sample code

---
+++ finding_leap_year.py
@@ -0,0 +1,13 @@
+year=int(input('enter the year'))
+# if year%4==0 and year%100!=0 or year%400==0:
+#     print('leap year')
+#     
+# else:
+#     print('Not a leap year')
+    
+if year%4==0 and year%100!=0:
+    print('A leap year')
+elif year%100==0 and year%400==0:
+    print('A leap year')
+else:
+    print('Not a leap year')
